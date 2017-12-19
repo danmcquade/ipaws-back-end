a5470637efe5a84fbeea47b01f4ab2e8f5267ca8
Dan McQuade <dtmcquade@gmail.com>
2017-12-19 10:04:23 -0500

    Update delete route

--- index.js
+++ index.js
@@ -59,8 +59,10 @@ app.post('/api/dogs/update/:id', (req, res) => {
 
 // Route to delete an indiviudal dog by ID
 app.get('/api/dogs/delete/:id', (req, res) => {
-  Dog.findOneAndRemove({ _id: req.params.id }).then(res.redirect('/'))
-  console.log('Dog has been deleted')
+  Dog.findOneAndRemove({ _id: req.params.id }).then(res.redirect('http://localhost:3000/dogs'))
+  .then((deleted) => {
+    console.log('Dog has been deleted')
+  })
 })
 
 // Route to post a new inquiry
