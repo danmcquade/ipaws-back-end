f7f515ed8c1804d039125f534e2bef4112f4dbad
Dan McQuade <dtmcquade@gmail.com>
2017-12-20 11:45:10 -0500

    Update connection.js to use local database when not running in production

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
-// mongoose.connect('mongodb://localhost/ipaws', {useMongoClient: true})
-mongoose.connect('mongodb://dbadmin:password123@ds161016.mlab.com:61016/ipaws', {useMongoClient: true})
+if (process.env.NODE_ENV === 'production') {
+  mongoose.connect(process.env.MLAB_URL, {useMongoClient: true})
+} else {
+  mongoose.connect('mongodb://localhost/ipaws', {useMongoClient: true})
+}
 
 mongoose.Promise = global.Promise
 
