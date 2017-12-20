9ae79494de6eac729ce888fd53401cee9a96e801
Dan McQuade <dtmcquade@gmail.com>
2017-12-20 14:17:14 -0500

    Update Connection.js to use mLab for MongoDB

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose')
 
-if (process.env.NODE_ENV === 'production') {
-  mongoose.connect(process.env.MLAB_URL, {useMongoClient: true})
-} else {
-  mongoose.connect('mongodb://localhost/ipaws', {useMongoClient: true})
-}
+mongoose.connect(process.env.MLAB_URL, {useMongoClient: true})
 
 mongoose.Promise = global.Promise
 
