fa4036d399d27c94dedbf94298602a95caba770a
Dan McQuade <dtmcquade@gmail.com>
2017-12-20 11:19:04 -0500

    Add mLab config and create Heroku instance

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/ipaws', {useMongoClient: true})
+// mongoose.connect('mongodb://localhost/ipaws', {useMongoClient: true})
+mongoose.connect('mongodb://dbadmin:password123@ds161016.mlab.com:61016/ipaws', {useMongoClient: true})
+
 mongoose.Promise = global.Promise
 
 const db = mongoose.connection
